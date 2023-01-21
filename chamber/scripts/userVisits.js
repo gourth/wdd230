3d1a3cab24e8e8917363a8d3b1a694e618c239a3
Garrett Ourth <garrettourth@gmail.com>
2023-01-21 10:01:43 -0600

    add visit counter

---
+++ chamber/scripts/userVisits.js
@@ -0,0 +1,24 @@
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let lastVisit = Number(window.localStorage.getItem(".today"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// show todays date.
+todayDisplay.textContent = Date.now();
+
+function getTimestampInDays () {
+    return Math.floor((timeNow.getTime() - lastVisit.getTime())/864000000)
+  }
