8ce521849ffb5ccbc6d22a3d2952d81e23e52784
Garrett Ourth <garrettourth@gmail.com>
2023-01-27 20:30:40 -0600

    fix form CSS

--- chamber/scripts/lastmodified.js
+++ chamber/scripts/lastmodified.js
@@ -5,3 +5,16 @@ document.getElementById('lastModified').textContent = `Last Modified: ${lastModi
 const copyrightDate = new Date(lastModifiedDate);
 
 document.getElementById('copyright').textContent = `©${copyrightDate.getFullYear()} Garrett Ourth USA`;
+
+
+
+var d = new Date();
+
+    // Set the value of the "date" field
+    document.getElementById("date").value = d.toDateString();
+
+    // Set the value of the "time" field
+    var hours = d.getHours();
+    var mins = d.getMinutes();
+    var seconds = d.getSeconds();
+    document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
