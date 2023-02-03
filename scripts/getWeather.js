557d8497043561738ea7b6837dd9fb95fdd8b1dc
Garrett Ourth <garrettourth@gmail.com>
2023-02-02 21:23:45 -0600

    add weather card

---
+++ scripts/getWeather.js
@@ -0,0 +1,26 @@
+const myTown = document.querySelector("#town")
+const myGraphic = document.querySelector("#graphic")
+const myDescription = document.querySelector("#description")
+const myTemperature = document.querySelector("#temperature")
+
+// const townName = "Trier"
+const myKey = "e06e780efdbb01434e6cb764177cbc72"
+const myLat = "29.757579860149058"
+const myLong = "-95.37387344212569"
+
+
+
+const myURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`
+
+fetch(myURL)
+  .then((response) => response.json())
+  .then((data) => displayData(data));
+
+  function displayData(data) {
+    console.log(data)
+    myTown.innerHTML = data.name
+    myGraphic.src=`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    myGraphic.alt=data.weather[0].main
+    myDescription.innerHTML = data.weather[0].description
+    myTemperature.innerHTML = `${data.main.temp}&deg;F`
+  }
