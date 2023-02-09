d78fc391d27863c2dacfce4b98340216200c574d
Garrett Ourth <garrettourth@gmail.com>
2023-02-08 22:11:11 -0600

    directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,10 +1,65 @@
-fetch('chamber/data/members.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+const url = 'chamber/data/members.json'
+const cards = document.querySelector(".cards")
+const jsonFile = 'data/members.json'
+
+async function apiFetch() {
+    try {
+        const response = await fetch(jsonFile);
+        if (response.ok) {
+            const data = await response.json();
+            displayBusinesses(data.businesses);
+        } else {
+            throw Error(await response.text());
+
+        }
+        } catch (error) {
+            console.log(error);
+        }
+     }
 
 
 
 
+    
+    const displayBusinesses = (businesses) => {
+        businesses.forEach((business) => {
+        let card = document.createElement("section");
+        let h2 = document.createElement("h2");
+        let image = document.createElement("img");
+        let infodiv = document.createElement("div");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let url = document.createElement("p");
+        let level = document.createElement("p");
+        let experience = document.createElement("p");
+
+        card.setAttribute("class", "busCard");
+        h2.textContent = `${business.name}`;
+        infodiv.setAttribute("class", "infodiv")
+
+        address.innerHTML = `Address: ${business.address}`;
+        phone.innerHTML = `Phone: ${business.phoneNumber}`;
+        url.innerHTML = `Website: <a href="http://${business.url}"> ${business.url}</a>`;
+        level.innerHTML = `Membership Level: ${business.membershipLvl}`;
+        experience.innerHTML = `Experience: ${business.yearsExp}`;
+
+        image.setAttribute("src", business.image);
+        image.setAttribute("alt", `Logo for ${business.name}`);
+        
+        card.appendChild(h2);
+        card.appendChild(image);
+        card.appendChild(infodiv);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+        card.appendChild(level);
+
+        document.querySelector("div.cards").appendChild(card);
+    }
+    )
+}
+    
+apiFetch();
 
 
 
