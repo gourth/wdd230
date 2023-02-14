903e195aef37219b4cdb2111d9e38d555b54063d
Garrett Ourth <garrettourth@gmail.com>
2023-02-13 22:20:16 -0600

    spotlight

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -5,26 +5,34 @@ function getSpotlightMembers() {
       .then(function(response) {
         return response.json();
       })
-      .then(function(business) {
-        // Filter members with Silver or Gold membership level
-        var qualifiedBusinesses = business.filter(function(business) {
-          return business.membershipLvl === 'Silver' || business.membershipLvl === 'Gold';
-        });
+      .then(function(myJsonObject) {  // Filter members with Silver or Gold membership level  
+        var allBusinesses = myJsonObject.businesses; var qualifiedBusinesses = allBusinesses.filter(function(business) {return business.membershipLvl === 'Silver' || business.membershipLvl === 'Gold';  });
       
         // Randomly pick three members
         var randomMembers = [];
         while (randomMembers.length < 3) {
           var randomIndex = Math.floor(Math.random() * qualifiedBusinesses.length);
-          if (!randomMembers.includes(qualifiedMembers[randomIndex])) {
-            randomMembers.push(qualifiedMembers[randomIndex]);
+          if (!randomMembers.includes(qualifiedBusinesses[randomIndex])) {
+            randomMembers.push(qualifiedBusinesses[randomIndex]);
           }
         }
       
         // Push data to HTML
         var spotlightContainer = document.getElementById('spotlight-container');
         randomMembers.forEach(function(business) {
-          var spotlightItem = document.createElement('div');
-          spotlightItem.innerHTML = 
+        //   var spotlightItem = document.createElement('div');
+
+            let card = document.createElement("section");
+            let name = document.createElement("h2");
+            let image = document.createElement("img");
+            let infodiv = document.createElement("div");
+            let address = document.createElement("p");
+            let phone = document.createElement("p");
+            let url = document.createElement("p");
+            let level = document.createElement("p");
+            let experience = document.createElement("p");
+        //   spotlightItem.innerHTML = business.name
+            name.innerHTML = `Name: ${business.name}`;
           address.innerHTML = `Address: ${business.address}`;
           phone.innerHTML = `Phone: ${business.phoneNumber}`;
           url.innerHTML = `Website: <a href="http://${business.url}"> ${business.url}</a>`;
@@ -34,6 +42,14 @@ function getSpotlightMembers() {
           image.setAttribute("src", business.image);
           image.setAttribute("alt", `Logo for ${business.name}`);
           spotlightContainer.appendChild(spotlightItem);
+
+        card.appendChild(name);
+        card.appendChild(image);
+        card.appendChild(infodiv);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+        card.appendChild(level);
         });
       })
       .catch(function(error) {
