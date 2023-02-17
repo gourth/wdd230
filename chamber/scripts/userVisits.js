fd17445ed38454834337773eabf725d425b91878
Garrett Ourth <garrettourth@gmail.com>
2023-02-16 18:35:22 -0600

    fix last visit on discover page

--- chamber/scripts/userVisits.js
+++ chamber/scripts/userVisits.js
@@ -1,24 +1,8 @@
-const todayDisplay = document.querySelector(".today");
-const visitsDisplay = document.querySelector(".visits");
-
-// get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
-let lastVisit = Number(window.localStorage.getItem(".today"));
-
-// determine if this is the first visit or display the number of visits.
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `This is your first visit!`;
-}
-
-// increment the number of visits.
-numVisits++;
-// store the new number of visits value
-localStorage.setItem("visits-ls", numVisits);
-// show todays date.
-todayDisplay.textContent = Date.now();
-
-function getTimestampInDays () {
-    return Math.floor((timeNow.getTime() - lastVisit.getTime())/864000000)
-  }
+const lastVisit = localStorage.getItem('lastVisit');
+        if (lastVisit) {
+            const daysSinceLastVisit = Math.round((Date.now() - lastVisit) / (1000 * 60 * 60 * 24));
+            document.getElementById('last-visit').textContent = `Last visit: ${daysSinceLastVisit} days ago`;
+        } else {
+            document.getElementById('last-visit').textContent = "This is your first visit!";
+        }
+        localStorage.setItem('lastVisit', Date.now());
