54971998cbd1e418a5c1ec44a23c92e88a211aa0
Garrett Ourth <garrettourth@gmail.com>
2023-02-17 20:49:01 -0600

    fix some issues

--- chamber/scripts/lastmodified.js
+++ chamber/scripts/lastmodified.js
@@ -1,20 +1,24 @@
-const lastModifiedDate = document.lastModified;
+// const lastModifiedDate = document.lastModified;
 
-document.getElementById('lastModified').textContent = `Last Modified: ${lastModifiedDate}`;
+// document.getElementById('lastModified').textContent = `Last Modified: ${lastModifiedDate}`;
 
-const copyrightDate = new Date(lastModifiedDate);
+// const copyrightDate = new Date(lastModifiedDate);
 
-document.getElementById('copyright').textContent = `©${copyrightDate.getFullYear()} Garrett Ourth USA`;
+// document.getElementById('copyright').textContent = `©${copyrightDate.getFullYear()} Garrett Ourth USA`;
 
 
 
-var d = new Date();
+// var d = new Date();
 
-    // Set the value of the "date" field
-    document.getElementById("date").value = d.toDateString();
+//     // Set the value of the "date" field
+//     document.getElementById("date").value = d.toDateString();
 
-    // Set the value of the "time" field
-    var hours = d.getHours();
-    var mins = d.getMinutes();
-    var seconds = d.getSeconds();
-    document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
+//     // Set the value of the "time" field
+//     var hours = d.getHours();
+//     var mins = d.getMinutes();
+//     var seconds = d.getSeconds();
+//     document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
+
+const lastModified = document.lastModified;
+const lastModifiedFooter = document.getElementById("lastModified");
+lastModifiedFooter.innerHTML = `Last Modified: ${lastModified}`;
